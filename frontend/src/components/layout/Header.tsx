d7444a5079daebee5f9a74be7612af6669b4541a
DEV VBOY <oluokunkabiru2015@gmail.com>
2026-09-18 22:53:16 +0100

    added the sociaety to link to home

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -51,8 +51,9 @@ export function Header() {
       <div className="hidden border-b border-plum-800/60 bg-plum-900 text-plum-200 md:block">
         <div className="mx-auto flex h-9 max-w-7xl items-center justify-between px-4 text-[0.78rem]">
           <p>
+          <Link to="/" className="text-gold-300 underline-offset-4 hover:underline">
+
             The Society{' '}
-            <Link to="/about#ican" className="text-gold-300 underline-offset-4 hover:underline">
               {loadingSettings ? (
                 <Skeleton className="inline-block h-3 w-40 align-middle bg-plum-700" />
               ) : (
